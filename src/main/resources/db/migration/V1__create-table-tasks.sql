c2d5f49812ede6851809b195005c63dd7157189b
vnniciusg <90412694+vnniciusg@users.noreply.github.com>
2023-11-22 16:44:33 -0300

    feat : Criação da migration e container para o banco

---
+++ src/main/resources/db/migration/V1__create-table-tasks.sql
@@ -0,0 +1,8 @@
+create table task(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title varchar(50),
+    description varchar(255),
+    start_date varchar(100),
+    end_date varchar(100),
+    status varchar(100)
+)
